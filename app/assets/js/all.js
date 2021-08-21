75c41a157cf3a480ba988c754b278c3b98edfdb6
debbyyeh <app2984@gmail.com>
2021-08-21 15:37:31 +0800

    final edit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,10 +1,10 @@
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  breakpoints: {
-    768: {
-      slidesPerView: 3,
-      spaceBetween: 30,
-    },
-  }
-});
+// var swiper = new Swiper(".mySwiper", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   breakpoints: {
+//     768: {
+//       slidesPerView: 3,
+//       spaceBetween: 30,
+//     },
+//   }
+// });
