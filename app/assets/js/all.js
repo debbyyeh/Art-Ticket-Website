66cb799f3a668153121fb058dc21b941736936f3
debbyyeh <app2984@gmail.com>
2021-08-22 23:47:07 +0800

    80% edit

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,10 +1,18 @@
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 3,
-//   spaceBetween: 30,
-//   breakpoints: {
-//     768: {
-//       slidesPerView: 3,
-//       spaceBetween: 30,
-//     },
-//   }
-// });
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    // direction: "horizonital",
+    // speed: 1000,
+    // loop: true,
+    // autoplay: {
+    //     delay: 1000,
+    //     // 手動滑動後，想要繼續自動輪播
+    //     disableOnInteraction: false,
+    // },
+    breakpoints: {
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    }
+});
